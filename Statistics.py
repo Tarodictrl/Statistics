76c2fb551177d975323d5bc0b193e13dfb1c8bc7
Tarodictrl <vudi600@gmail.com>
2022-09-14 01:06:02 +0700

    Add comments.

--- Statistics.py
+++ Statistics.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 
 
 def percentage(part: int, whole: int) -> float:
+    """
+    Функция расчета процента.
+    :param part:
+    :param whole:
+    :return:
+    """
     return 100 * float(part) / float(whole)
 
 
@@ -12,9 +18,15 @@ class Statistics:
                  outputDatasetSex: str,
                  outputDatasetRelations: str,
                  firstNumberSampling: int,
-                 samplingStep: int,
-                 gender: str):
-
+                 samplingStep: int):
+        """
+        Функция обработки набора данных.
+        :param inputDataset: Исходный файл набора данных.
+        :param outputDatasetSex: Выходной файл массива полов.
+        :param outputDatasetRelations: Выходной файл массива процентов.
+        :param firstNumberSampling: Начальное число размера выборки.
+        :param samplingStep: Шаг выборки.
+        """
         self.relations = []
         self.datasetSex = []
         self.selections = []
@@ -23,9 +35,13 @@ class Statistics:
         self.outputDatasetRelations = outputDatasetRelations
         self.firstNumberSampling = firstNumberSampling
         self.samplingStep = samplingStep
-        self.gender = gender
 
     def GetDatasetSex(self, overwrite: bool):
+        """
+        Функция обработки исходного набора данных.
+        :param overwrite: Переписать файл.
+        :return: Массив полов.
+        """
         with open(self.inputDataset, newline='') as f:
             reader = csv.DictReader(f)
             self.datasetSex = [row['SEX'] for row in reader]
@@ -35,6 +51,11 @@ class Statistics:
                 pickle.dump(self.datasetSex, f)
 
     def GetDatasetRelations(self, overwrite: bool):
+        """
+        Функция получения процентов от размера выборки.
+        :param overwrite: Переписать файл.
+        :return: Массив процентов.
+        """
         self.selections = [s for s in range(self.firstNumberSampling,
                                             len(self.datasetSex), self.samplingStep)]
         if overwrite:
@@ -42,9 +63,8 @@ class Statistics:
                 sel = self.datasetSex[0:i]
                 genderCount = 0
                 for s in sel:
-                    if s == self.gender: genderCount += 1
+                    if s == "F": genderCount += 1
                 self.relations.append(percentage(genderCount, len(sel)))
-
             with open(self.outputDatasetRelations, 'wb') as f:
                 pickle.dump(self.relations, f)
         else:
@@ -52,8 +72,14 @@ class Statistics:
                 self.relations = pickle.load(f)
 
     def Plotting(self, sizeByWidth: int, sizeByHeight: int, outputImage: str):
+        """
+        Функция отрисовки графика.
+        :param sizeByWidth: Размер по ширине.
+        :param sizeByHeight: Размер по высоте.
+        :param outputImage: Название выходного файла.
+        :return: График.
+        """
         figure, ax = plt.subplots(2, 1)
-
         ax[0].ticklabel_format(useOffset=False, style='plain')
         ax[0].scatter(self.selections, self.relations)
         ax[0].set_title("Процент женщин от общего количества")
@@ -65,7 +91,6 @@ class Statistics:
         for a in ax:
             a.set_xlabel("Размер выборки")
             a.set_ylabel("Процент женщин")
-
         figure.set_figwidth(sizeByWidth)
         figure.set_figheight(sizeByHeight)
 
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import Statistics
 
 if __name__ == '__main__':
     statistics = Statistics.Statistics("Datasets/WholeYear.csv", "Datasets/DatasetSex.pkl",
-                                       "Datasets/DatasetRelations.pkl", 100, 100, "F")
+                                       "Datasets/DatasetRelations.pkl", 100, 100)
     statistics.GetDatasetSex(False)
     statistics.GetDatasetRelations(False)
-    statistics.Plotting(20, 20, "image.png")
+    statistics.Plotting(10, 10, "image.png")
 
